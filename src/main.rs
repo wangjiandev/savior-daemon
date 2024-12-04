e6a665f203ee0374bf34e80af7d78acae731d787
wangjian <wangjian0504@gmail.com>
2024-12-04 18:08:42 +0800

    test: 使用crates包实现获取程序数据目录

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,38 @@
 use anyhow::Result;
+use platform_dirs::AppDirs;
 use savior_daemon::{
     channel::redis_tool::Ctx,
     process::{listener::listener_process, register::register_process},
 };
-use std::{
-    env,
-    path::{Path, PathBuf},
-};
 
 fn main() -> Result<()> {
-    // let ctx = Ctx::new();
-    // let register_handle = register_process(&ctx)?;
-    // let listener_handle = listener_process(&ctx)?;
-    // let _ = register_handle.join().unwrap();
-    // let _ = listener_handle.join().unwrap();
+    let ctx = Ctx::new();
+    let register_handle = register_process(&ctx)?;
+    let listener_handle = listener_process(&ctx)?;
+    let _ = register_handle.join().unwrap();
+    let _ = listener_handle.join().unwrap();
 
     // 获取当前用户的AppData/Roaming
-    get_app_data_dir()?;
+    let app_dirs = AppDirs::new(Some("Savior"), false).unwrap();
+    let home = app_dirs.data_dir;
 
-    // #[allow(deprecated)]
-    // let home = home_dir().unwrap();
-    // println!("home_dir is {:?}", home);
+    println!("home: {}", home.display());
+
     Ok(())
 }
 
-#[cfg(windows)]
-fn get_app_data_dir() -> Result<PathBuf> {
-    let home = env::var("APPDATA")?;
-    let path = Path::new(&home).join("Savior");
-    println!("path: {}", path.display());
-    Ok(path)
-}
+// #[cfg(windows)]
+// fn get_app_data_dir() -> Result<PathBuf> {
+//     let home = env::var("APPDATA")?;
+//     let path = Path::new(&home).join("Savior");
+//     println!("path: {}", path.display());
+//     Ok(path)
+// }
 
-#[cfg(not(windows))]
-fn get_app_data_dir() -> Result<PathBuf> {
-    let home = env::var("HOME")?;
-    let path = Path::new(&home).join(".Savior");
-    println!("path: {}", path.display());
-    Ok(path)
-}
+// #[cfg(not(windows))]
+// fn get_app_data_dir() -> Result<PathBuf> {
+//     let home = env::var("HOME")?;
+//     let path = Path::new(&home).join(".Savior");
+//     println!("path: {}", path.display());
+//     Ok(path)
+// }
